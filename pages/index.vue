d08a5d7badef1bbb7b0ae4183da1ff295dcfd6b2
Edward <fanofparadox@gmail.com>
2023-03-25 19:01:46 +0300

    mainpage works

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,121 @@
         <p>Елена Руденко, практикующий психолог</p>
       </div>
     </article>
-    <article id="art-therapy"></article>
-    <article id="about-me"></article>
-    <article id="online"></article>
-    <article id="appointments"></article>
-    <article id="contact"></article>
+    <article id="art-therapy">
+      <div class="max-width-wrap">
+        <h1 class="article-h1">Арт-терапия</h1>
+        <div class="divider"></div>
+        <p class="article-p">
+          Арт-терапия - это подход терапии, использующий художественные материалы, творческий процесс, 
+          образ и диалог, направленный на самовыражение, самопознание и/или изменение консультируемого человека. 
+          Арт-терапевт является обученным и аккредитованным профессионалом, который этично и в безопасной среде способствует этому процессу.
+        </p>
+        <div class="grid-wrap">
+          <div class="grid-element">
+            <h4 class="article-h4">Преимущества арт-терапии</h4>
+            <p class="article-p">Любой может стать художником своего пути к благополучию.</p>
+            <p class="article-p">Арт-терапия — это увлекательный и интуитивный способ самовыражения.</p>
+            <p class="article-p">Вам не нужно быть профессиональным художником, чтобы извлечь пользу из терапевтических качеств искусства.</p>
+          </div>
+          <div class="grid-element">
+            <h4 class="article-h4">Как я могу помочь Вам</h4>
+            <p class="article-p">
+              Индивидуально подобранные онлайн-сеансы арт-терапии для людей, которым нужна поддержка при преодолении беспокойства, депрессии, 
+              стресса, потери, травмы и других проблем с психическим здоровьем и благополучием.
+            </p>
+            <p class="article-p">Вы увидите разницу. Одна картинка стоит тысячи слов. Пусть Ваш образ говорит за Вас и с Вами.</p>
+          </div>
+        </div>
+      </div>
+    </article>
+    <article id="about-me">
+      <div class="max-width-wrap">
+        <h1 class="article-h1">Обо мне</h1>
+        <div class="divider"></div>
+        <div class="grid-wrap">
+          <img src="@/assets/img/rudenko.webp" alt="img">
+          <div id="about-me-text-wrap">
+            <h4 class="article-h4">Елена Руденко, арт-терапевт, дипломированный специалист</h4>
+            <p class="article-p">
+              Я лицензированный арт-терапевт, который изучал и применял терапевтические качества искусства в течение последних 3 лет. 
+              Я работаю в сотрудничестве со своими клиентами, мягко направляя и поддерживая их в процессе выздоровления. 
+              Моя главная роль заключалась в том, чтобы показать моим клиентам, как их мысли, чувства и переживания можно безопасно описать, 
+              сдержать и сформировать с помощью их произведений искусства.
+            </p>
+            <ul>
+              <li>
+                <strong>Опыт:</strong>
+                Индивидуальный и групповой, с 2020 года
+              </li>
+              <li>
+                <strong>Профессиональное участие:</strong>
+                РПО, Арт-терапевтическая ассоциация
+              </li>
+              <li>
+                <strong>Языки:</strong>
+                Русский, румынский, английский
+              </li>
+              <li>
+                <strong>Возрастные группы:</strong>
+                Подростки, взрослые и пожилые люди
+              </li>
+              <li>
+                <strong>Специальность:</strong>
+                Беспокойство, стресс, депрессия, самооценка, способность справляться с трудностями, 
+                иммиграция, потеря, травма, привязанность, зависимость, нарушения развития
+              </li>
+              <li>
+                Я <strong>не</strong> ставлю диагноз. Я могу предложить оценку на основе арт-терапии и помочь в общей описательной диагностике.
+              </li>
+              <li>
+                Я <strong>не</strong> навязываю интерпретацию произведению. 
+                Я направляю своих клиентов, чтобы помочь им интерпретировать их собственное искусство и установить связь с их жизненным опытом.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </article>
+    <article id="online">
+      <div class="max-width-wrap">
+        <h1 class="article-h1">Арт-терапия онлайн</h1>
+        <div class="divider"></div>
+        <div class="grid-wrap">
+          <div id="online-text-wrap">
+            <h4 class="article-h4">Индивидуальные сессии (Онлайн)</h4>
+            <p class="article-p">Сессии адаптированы для удовлетворения индивидуальных потребностей и целей.</p>
+            <p class="article-p">Они будут включать в себя создание произведений искусства, дискуссии и размышления.</p>
+            <p class="article-p">Рекомендованные для терапии вещи:</p>
+            <ul>
+              <li>Ручка</li>
+              <li>Карандаш</li>
+              <li>Фломастеры</li>
+              <li>Маркеры</li>
+              <li>Краски</li>
+              <li>Пластелин</li>
+              <li>Другие интересные Вам материалы</li>
+            </ul>
+          </div>
+          <img src="@/assets/img/art.webp" alt="img">
+        </div>
+      </div>
+    </article>
+    <article id="contact">
+      <div class="max-width-wrap">
+        <h1 class="article-h1">Контакты</h1>
+        <div class="divider"></div>
+        <h4 class="article-h4">Отправьте сообщение</h4>
+        <h4 class="article-h4">Индивидуальная забота, которую Вы заслуживаете</h4>
+        <p class="article-p">
+          Пожалуйста, свяжитесь со мной напрямую с любыми вопросами, комментариями или запросами по расписанию, которые могут у Вас возникнуть.
+        </p>
+        <h4 class="article-h4" :style="{ 'margin-top': 24 + 'px'}">Арт-терапия и самосовершенстование</h4>
+        <p class="article-p">+7 (977) 796-15-57</p>
+        <h4 class="article-h4" :style="{ 'margin-top': 24 + 'px'}">Часы приема</h4>
+        <p class="article-p">Понедельник-пятница: с 09:00 до 18:00 МСК (кроме как по записи)</p>
+        <p class="article-p">Суббота: только по записи</p>
+      </div>
+    </article>
   </main>
   <footer></footer>
 </template>
@@ -41,13 +151,18 @@ body {
 
 #get-started {
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: center;
   padding: 100px 24px 40px;
   background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.25) 100%), url(@/assets/img/background.webp);
   background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
+}
+
+#get-started > .max-width-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 #get-started h1 {
@@ -72,6 +187,8 @@ body {
 
 #get-started a {
   display: block;
+  box-sizing: border-box;
+  max-width: 320px;
   margin: 24px 0;
   border-radius: 4px;
   padding: 10px 32px;
@@ -88,6 +205,142 @@ body {
 
 #get-started a:hover {
   background-color: rgb(236, 183, 160);
+}
+
+#art-therapy {
+  display: flex;
+  justify-content: center;
+  padding: 40px 24px;
+  background-color: rgb(246, 246, 246);
+}
+
+#art-therapy > .max-width-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.article-h1 {
+  margin: 0;
+  font-family: Montserrat Bold, sans-serif;
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 45px;
+  text-align: center;
+  color: rgb(209, 128, 82);
+}
+
+.divider {
+  width: 50px;
+  box-sizing: border-box;
+  margin: 26px 0 40px 0;
+  border-bottom: 2px solid rgb(226, 226, 226);
+}
+
+.article-p {
+  margin: 0;
+  font-family: OpenSans Regular, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  text-align: center;
+  color: rgb(94, 94, 94);
+}
+
+.article-h4 {
+  margin: 0 0 24px 0;
+  font-family: OpenSans Regular, sans-serif;
+  font-size: 22px;
+  font-weight: 400;
+  line-height: 28px;
+  text-align: center;
+  color: rgb(27, 27, 27);
+}
+
+#art-therapy .grid-wrap {
+  display: grid;
+  grid-template-columns: 1fr;
+}
+
+#art-therapy .grid-element {
+  padding: 16px 0 0 0;
+}
+
+#about-me {
+  display: flex;
+  justify-content: center;
+  padding: 40px 24px;
+}
+
+#about-me > .max-width-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#about-me img {
+  width: 100%;
+  height: calc((100vw - 48px)/1.33);
+  margin-bottom: 24px;
+}
+
+#about-me .grid-wrap {
+  display: grid;
+  grid-template-columns: 1fr;
+}
+
+#about-me ul {
+  margin: 24px 0;
+  font-family: OpenSans Regular, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  text-align: left;
+  color: rgb(87, 87, 87);
+}
+
+#online {
+  display: flex;
+  justify-content: center;
+  padding: 40px 24px;
+  background-color: rgb(246, 246, 246);
+}
+
+#online .grid-wrap {
+  display: grid;
+  grid-template-columns: 1fr;
+}
+
+#online > .max-width-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#online ul {
+  font-family: OpenSans Regular, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  text-align: left;
+  color: rgb(87, 87, 87);
+}
+
+#online img {
+  width: 100%;
+  height: calc((100vw - 48px)/2);
+}
+
+#contact {
+  display: flex;
+  justify-content: center;
+  padding: 40px 24px;
+}
+
+#contact > .max-width-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 </style>
